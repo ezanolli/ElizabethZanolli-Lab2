6c369bdcced60a963d955dd94ccca06486e584bb
ezanolli <97760201+ezanolli@users.noreply.github.com>
2022-02-06 21:01:26 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+  This web service measures the distance between two places. 
+  The latitude and longitude of each location is the output, 
+  and the distance measured in kilometers is the output.
+
+  Input URL:
+  https://ww1go.sse.codesandbox.io/?latA=36.16318&lonA=-115.18600&latB=39.30063&lonB=-76.62771
+  Location names: Las Vegas, NV and Baltimore, MD
+  Output distance: 3383.14 km
+*/
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
